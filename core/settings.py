fe9a279344b2cd11383e4c72b27ab760418513fb
naiv <nazarivankiv1@gmail.com>
2023-11-24 14:01:51 +0200

    removed insecure secret key for deployment

--- core/settings.py
+++ core/settings.py
@@ -24,16 +24,12 @@ CORE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    "django-insecure-z0%+enw=6tr@l7w9djf(jv#qrgbveh&+e!q#yndnzjhru8#i70"
-)
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = "RENDER" not in os.environ
 
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = []
 
 # Assets Management
 ASSETS_ROOT = os.getenv("ASSETS_ROOT", "/static/assets")
