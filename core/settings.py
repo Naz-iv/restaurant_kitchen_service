0f63d1b762361c7744d2d2934551b21d4d6f31de
Nazar Ivankiv <83316497+Naz-iv@users.noreply.github.com>
2023-11-24 16:18:58 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -77,7 +77,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "kitchen_service.context_processors.stats_context_processor"
             ],
         },
     },
