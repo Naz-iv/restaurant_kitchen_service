b8d02ec81ae705e9e5bde74d13fcba6530f9c71f
naiv <nazarivankiv1@gmail.com>
2023-11-24 15:21:16 +0200

    added https://restaurantkitchenservice-ir76.onrender.com to allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
-    "https://restaurantkitchenservice-ir76.onrender.com",
+    "restaurantkitchenservice-ir76.onrender.com",
 ]
 
 # Assets Management
